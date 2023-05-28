37272c3e9cba8aaff70338c9896354009880096d
JmRss <jmr010487@gmail.com>
2023-05-28 18:02:05 +0200

    Add Navbar responsive

---
+++ script/script.js
@@ -0,0 +1,36 @@
+const text = document.querySelector('.text_appear');
+const toggleMenuBtn = document.querySelector('#menu-btn');
+const toggleMenuImg = document.querySelector('#menu-btn img');
+const toggledMenu = document.querySelector('#toggled-menu');
+const menuLinks = document.querySelector('#main-nav ul a');
+
+////event navbar responsive /////
+toggleMenuBtn.addEventListener('click', toggleNav);
+
+function toggleNav() {
+  toggledMenu.classList.toggle('-translate-y-full');
+
+  if (toggledMenu.classList.contains('-translate-y-full')) {
+    toggleMenuImg.setAttribute('src', 'assets/menu.svg');
+    toggleMenuBtn.setAttribute('aria-expanded', 'false');
+  } else {
+    toggleMenuImg.setAttribute('src', 'assets/cross.svg');
+    toggleMenuBtn.setAttribute('aria-expanded', 'true');
+  }
+}
+//// text appear /////
+new Typewriter(text, {
+  //animation loop
+  loop: true,
+  deleteSpeed: 20,
+})
+  .changeDelay(50)
+  .typeString('Cuisinez :<span style="color: #97F03F">  équilibré,</span>')
+  .pauseFor(500)
+  .deleteChars(10)
+  .typeString('<span style="color: #44FCEA">facilement,</span>')
+  .pauseFor(500)
+  .deleteChars(11)
+  .typeString('<span style="color: #FF6B00">rapidement</strong> !!!</span>')
+  .pauseFor(2000)
+  .start();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ['./index.html', './script/script.js*'],
   theme: {
     fontFamily: {
       lobster: ['Lobster', 'cursive'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        colorNav: 'rgba(173, 55, 29, 0.68)',
+      },
+    },
   },
   plugins: [],
 };
