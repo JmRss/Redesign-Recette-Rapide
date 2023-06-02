358df45ad7ca1ceb26b4c8c0c90716169d4c9fb7
JmRss <jmr010487@gmail.com>
2023-06-02 15:06:18 +0200

    Responsive OK

--- script/index.js
+++ script/index.js
@@ -1,21 +1,21 @@
-const text = document.querySelector('.text_appear');
-const toggleMenuBtn = document.querySelector('#menu-btn');
-const toggleMenuImg = document.querySelector('#menu-btn img');
-const toggledMenu = document.querySelector('#toggled-menu');
-const menuLinks = document.querySelector('#main-nav ul a');
+const text = document.querySelector('.text_appear')
+const toggleMenuBtn = document.querySelector('#menu-btn')
+const toggleMenuImg = document.querySelector('#menu-btn img')
+const toggledMenu = document.querySelector('#toggled-menu')
+const menuLinks = document.querySelector('#main-nav ul a')
 ////event navbar responsive /////
-toggleMenuBtn.addEventListener('click', toggleNav);
+toggleMenuBtn.addEventListener('click', toggleNav)
 
 /// function navbar ///
 function toggleNav() {
-  toggledMenu.classList.toggle('-translate-y-full');
+  toggledMenu.classList.toggle('-translate-y-full')
 
   if (toggledMenu.classList.contains('-translate-y-full')) {
-    toggleMenuImg.setAttribute('src', 'assets/menu.svg');
-    toggleMenuBtn.setAttribute('aria-expanded', 'false');
+    toggleMenuImg.setAttribute('src', 'assets/menu.svg')
+    toggleMenuBtn.setAttribute('aria-expanded', 'false')
   } else {
-    toggleMenuImg.setAttribute('src', 'assets/cross.svg');
-    toggleMenuBtn.setAttribute('aria-expanded', 'true');
+    toggleMenuImg.setAttribute('src', 'assets/cross.svg')
+    toggleMenuBtn.setAttribute('aria-expanded', 'true')
   }
 }
 
@@ -35,5 +35,5 @@ if (text) {
     .deleteChars(11)
     .typeString('<span style="color: #FF6B00">rapidement</strong> !!!</span>')
     .pauseFor(2000)
-    .start();
+    .start()
 }
